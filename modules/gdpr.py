ea74dbe3983dafc07125d6ce346e697b1f8f29a4
wolfricoz <ricoisdenaam@gmail.com>
2024-09-07 03:35:11 +0200

    Made the messaged ephemeral, so they are private to the user themselves.

--- modules/gdpr.py
+++ modules/gdpr.py
@@ -24,14 +24,14 @@ class gdpr(commands.GroupCog):
 
         dev = self.bot.get_user(188647277181665280)
         await dev.send(f"User {interaction.user.mention}({interaction.user.id}) has requested data removal, please wait for them to contact.")
-        await send_response(interaction, "Please contact the developer `ricostryker` to request data removal or join our [support server](https://discord.gg/5tcpArff) and open a ticket.")
+        await send_response(interaction, "Please contact the developer `ricostryker` to request data removal or join our [support server](https://discord.gg/5tcpArff) and open a ticket.", ephemeral=True)
 
     @app_commands.command(name="data", description="Request your data")
     async def data(self, interaction: discord.Interaction):
         """Returns user data"""
         dev = self.bot.get_user(188647277181665280)
         await dev.send(f"User {interaction.user.mention}({interaction.user.id}) has requested data, please wait for them to contact.")
-        await send_response(interaction, "Please contact the developer `ricostryker` to request your data or join our [support server](https://discord.gg/5tcpArff) and open a ticket.")
+        await send_response(interaction, "Please contact the developer `ricostryker` to request your data or join our [support server](https://discord.gg/5tcpArff) and open a ticket.", ephemeral=True)
 
 
 
