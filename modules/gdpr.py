d6f97b6debce59176fc9af36526d909e94616d4d
wolfricoz <ricoisdenaam@gmail.com>
2024-09-07 03:23:33 +0200

    Added GDPR commands to the bot to make it easier for users to contact the devs.

--- modules/gdpr.py
+++ modules/gdpr.py
@@ -5,7 +5,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from classes.databaseController import ConfigData
+from classes.databaseController import ConfigData, UserTransactions
 from classes.helpers import has_onboarding, invite_info
 from classes.support.discord_tools import send_response
 
@@ -17,6 +17,11 @@ class gdpr(commands.GroupCog):
     @app_commands.command(name="removal", description="Request removal of your data")
     async def removal(self, interaction: discord.Interaction):
         """Removes user data"""
+        user = UserTransactions.get_user(interaction.user.id)
+        if user is None or user.date_of_birth is None:
+            await send_response(interaction, "No data found for you.")
+            return
+
         dev = self.bot.get_user(188647277181665280)
         await dev.send(f"User {interaction.user.mention}({interaction.user.id}) has requested data removal, please wait for them to contact.")
         await send_response(interaction, "Please contact the developer `ricostryker` to request data removal or join our [support server](https://discord.gg/5tcpArff) and open a ticket.")
