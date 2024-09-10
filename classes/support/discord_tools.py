a967cac8d7620a5c7a4110af11f767d9d5739ee1
wolfricoz <ricoisdenaam@gmail.com>
2024-09-10 19:44:07 +0200

    Added more error handling to send_response. respond->followup->channel

--- classes/support/discord_tools.py
+++ classes/support/discord_tools.py
@@ -57,7 +57,12 @@ async def send_response(interaction: discord.Interaction, response, ephemeral=Fa
         await interaction.guild.owner.send(f"Missing permission to send message to {channel.name}. Check permissions: {', '.join(missing_perms)}", )
         raise NoMessagePermission(missing_permissions=missing_perms)
     except discord.errors.NotFound or discord.errors.InteractionResponded:
-        await interaction.followup.send(
-                response,
-                ephemeral=ephemeral
-        )
+        try:
+            await interaction.followup.send(
+                    response,
+                    ephemeral=ephemeral
+            )
+        except discord.NotFound:
+            logging.error("Channel not found")
+            await send_message(interaction.channel, response)
+
