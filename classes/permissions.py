e80aa6cb98463530ea08f7b9cb67f8224a2ec80e
wolfricoz <ricoisdenaam@gmail.com>
2024-09-16 03:24:59 +0200

    Added developer overrides on permissions

--- classes/permissions.py
+++ classes/permissions.py
@@ -1,9 +1,15 @@
+import os
+
 import discord
 from discord import app_commands
 from discord.ext import commands
 
 from classes.databaseController import ConfigData
 
+
+def check_dev(userid):
+    if int(userid) == int(os.getenv("DEVELOPER")):
+        return True
 
 def check_admin(user: discord.Member):
     adminroles = ConfigData().get_key(user.guild.id, 'admin')
@@ -32,6 +38,8 @@ def check_roles_admin():
 
 def check_app_roles():
     async def pred(interaction: discord.Interaction):
+        if check_dev(interaction.user.id):
+            return True
         modroles = ConfigData().get_key(interaction.guild.id, 'mod')
         adminroles = ConfigData().get_key(interaction.guild.id, 'admin')
         user_roles = [x.id for x in interaction.user.roles]
@@ -42,6 +50,8 @@ def check_app_roles():
 
 def check_app_roles_admin():
     async def pred(interaction):
+        if check_dev(interaction.user.id):
+            return True
         adminroles = ConfigData().get_key(interaction.guild.id, 'admin')
         user_roles = [x.id for x in interaction.user.roles]
         return any(x in adminroles for x in user_roles)
