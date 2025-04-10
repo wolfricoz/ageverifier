05e1d24cb0da4a58a6bd8d5459d207d34d2e89b7
ricoi <ricoisdenaam@gmail.com>
2025-04-10 22:50:00 +0200

    Increased shard count

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ intents.message_content = True
 intents.members = True
 activity = discord.Activity(type=discord.ActivityType.watching, name="over the community")
 
-bot = commands.AutoShardedBot(command_prefix=PREFIX, case_insensitive=False, intents=intents, activity=activity)
+bot = commands.AutoShardedBot(command_prefix=PREFIX, case_insensitive=False, intents=intents, activity=activity, shard_count=5)
 
 bot.DEV = int(os.getenv("DEV"))
 
