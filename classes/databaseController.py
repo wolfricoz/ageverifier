fbe5bbed111ee5aeb76a3f0ab85c55b41f8ba4a8
wolfricoz <ricoisdenaam@gmail.com>
2025-06-27 13:09:40 +0200

    Transplanting staff module from banwatch to ageverifier

--- classes/databaseController.py
+++ classes/databaseController.py
@@ -10,6 +10,7 @@ from sqlalchemy import and_
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import Select
+from sqlalchemy.util import to_list
 
 import databases.current as db
 from classes.encryption import Encryption
@@ -698,3 +699,28 @@ class ServerTransactions() :
 			if reload :
 				ConfigData().load_guild(guild_id)
 		return
+
+
+class StaffDbTransactions(DatabaseTransactions) :
+
+	def get(self, uid: int) -> Staff | None :
+		return session.scalar(Select(Staff).where(Staff.uid == uid))
+
+	def add(self, uid: int, role: str) -> Staff | None :
+		if self.get(uid) :
+			return None
+		new_staff = Staff(uid=uid, role=role.lower())
+		session.add(new_staff)
+		self.commit(session)
+		return new_staff
+
+	def get_all(self) -> list[Staff] :
+		return to_list(session.scalars(Select(Staff)).all())
+
+	def delete(self, uid: int) -> bool :
+		staff = self.get(uid)
+		if not staff :
+			return False
+		session.delete(staff)
+		self.commit(session)
+		return True
