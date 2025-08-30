ebb552872972057e8a2120bfdef9565ffbd4d95b
ricoi <ricoisdenaam@gmail.com>
2025-08-30 21:59:24 +0200

    Live debugging

--- databases/controllers/ButtonTransactions.py
+++ databases/controllers/ButtonTransactions.py
@@ -26,6 +26,8 @@ class LobbyDataTransactions(DatabaseTransactions):
             The newly created LobbyData object.
         """
         with self.createsession() as session:
+            logging.info(dob)
+            logging.info(Encryption().encrypt(dob))
             new_entry = LobbyData(
                 uuid=uuid,
                 uid=user_id,
