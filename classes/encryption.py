99ba937bff6b1bb54d0bf3d63eab9ccc208e797b
ricoi <ricoisdenaam@gmail.com>
2025-09-01 08:01:06 +0200

    Removed logging from encryption module

--- classes/encryption.py
+++ classes/encryption.py
@@ -20,11 +20,8 @@ class Encryption:
         """Decrypts the text"""
         if text is None:
             return "No Dob Stored"
-        logging.info(f"Decrypting {text}")
         if text.startswith("b'") and text.endswith("'") :
           text = text[2 :-1]
         if text.startswith('b"') and text.endswith('"') :
           text = text[2 :-1]
-        logging.info(self.fernet)
-        logging.info(EncryptionKey)
         return self.fernet.decrypt(text).decode()
