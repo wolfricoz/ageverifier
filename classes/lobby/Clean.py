b76e544f3cddbd2013716564e5d1dc61e1c7a896
ricoi <ricoisdenaam@gmail.com>
2026-06-07 21:49:17 +0200

    Cleaned up some code and double checked it.

--- classes/lobby/Clean.py
+++ classes/lobby/Clean.py
@@ -35,25 +35,29 @@ async def clean_lobby(bot, guild: discord.Guild, custom_days = None) :
 	removal_date = datetime.now(tz=UTC) - timedelta(days=days)
 
 	async for message in lobby_channel.history(limit=None, before=removal_date) :
-		logging.info(f"Message: {message.content}")
-		if not message.author :
-			logging.warning(f"[clean-up] No author in {message.author}")
-			Queue().add(message.delete())
-		if (not message.author.bot and message.author.bot != bot.user) or len(message.mentions) < 1 :
-			continue
-		if message.author != bot.user and isinstance(message.author, discord.Member) and message.author.guild_permissions.manage_messages :
-			continue
-		user = message.mentions[0]
-		if isinstance(user, discord.Member) and user.guild_permissions.manage_messages :
-			continue
-		count_messages += 1
-		if user.global_name not in kicked_users :
-			kicked_users.append(user.global_name)
-		if not DEBUG :
-			Queue().add(user.send(removal_message), 0)
-			if isinstance(user, discord.Member) :
-				Queue().add(user.kick(reason=f"In lobby for more than {days} days"), 0)
-			Queue().add(message.delete(), 0)
+		try:
+			logging.info(f"Message: {message.content}")
+			if not message.author or not isinstance(message.author, discord.Member) :
+				logging.warning(f"[clean-up] No author in {message.author}")
+				Queue().add(message.delete())
+				continue
+			if not message.author.bot or message.author != bot.user or len(message.mentions) < 1 :
+				continue
+			if message.author != bot.user and isinstance(message.author, discord.Member) and message.author.guild_permissions.manage_messages :
+				continue
+			user = message.mentions[0]
+			if isinstance(user, discord.Member) and user.guild_permissions.manage_messages :
+				continue
+			count_messages += 1
+			if user.global_name not in kicked_users :
+				kicked_users.append(user.global_name)
+			if not DEBUG :
+				Queue().add(user.send(removal_message), 0)
+				if isinstance(user, discord.Member) :
+					Queue().add(user.kick(reason=f"In lobby for more than {days} days"), 0)
+				Queue().add(message.delete(), 0)
+		except Exception as e:
+			logging.error(f"[clean-up] {guild.name}: {e}")
 	if count_messages < 1 and len(kicked_users) < 1 :
 		return
 
