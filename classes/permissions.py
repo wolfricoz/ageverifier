f6ae1170f3d9de2b1b18b287a8ebfaf51cb0b471
ricoi <ricoisdenaam@gmail.com>
2026-07-16 21:28:39 +0200

    [Code Audit] Cleaned up permissions

--- classes/permissions.py
+++ classes/permissions.py
@@ -6,10 +6,12 @@ from databases.transactions.ConfigData import ConfigData
 
 
 def check_dev(userid):
-    if int(userid) == int(os.getenv("DEVELOPER")):
-        return True
+    return int(userid) == int(os.getenv("DEVELOPER"))
+
 
 def check_admin(user: discord.Member):
     adminroles = ConfigData().get_key(user.guild.id, 'admin')
+    if not adminroles:
+        return False
     user_roles = [x.id for x in user.roles]
     return any(x in adminroles for x in user_roles)
