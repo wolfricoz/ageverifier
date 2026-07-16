468729b5f697773ca9f72f18bd723d92f92b723a
ricoi <ricoisdenaam@gmail.com>
2026-07-16 22:03:01 +0200

    [Code Audit] Moved to owner_id, this is always available.

--- classes/blacklist.py
+++ classes/blacklist.py
@@ -1,9 +1,9 @@
 import logging
 
 import discord
+from discord_py_utilities.messages import send_message
 
 from classes.jsonmaker import Configer
-from discord_py_utilities.messages import send_message
 from classes.support.queue import Queue
 
 
@@ -15,8 +15,9 @@ async def blacklist_check(guild: discord.Guild, log: discord.TextChannel) -> boo
         await guild.leave()
         # await log.send(f"[DEV MODE] Leaving disabled for testing")
         return True
+
     if await Configer.is_user_blacklisted(guild.owner.id):
-        logging.info(f"Leaving {guild.name} because the {guild.owner.name}({guild.owner.id}) is blacklisted")
+        logging.info(f"Leaving {guild.name} because the {guild.owner.name}({guild.owner_id}) is blacklisted")
         Queue().add(send_message(log, f"Leaving {guild}({guild.id}) because the {guild.owner.name}({guild.owner.id}) is blacklisted"))
         await Configer.add_to_blacklist(guild.id)
         # await log.send(f"[DEV MODE] Leaving disabled for testing")
